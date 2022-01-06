43777ab05bd7ca6141af95dfc677c4fe97271e85
Ronny Matute <rgmatute91@gmail.com>
2022-01-05 21:35:16 -0500

    agregue lombok

---
+++ src/main/java/com/github/rgmatute91/entity/lombok/User.java
@@ -0,0 +1,66 @@
+package com.github.rgmatute91.entity.lombok;
+
+
+public class User {
+
+	private String usuario;
+
+	private String nombre;
+
+	private String apellidos;
+
+	private String contrasenia;
+
+	private String correo;
+
+	private String foto;
+
+	public String getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellidos() {
+		return apellidos;
+	}
+
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+
+	public String getContrasenia() {
+		return contrasenia;
+	}
+
+	public void setContrasenia(String contrasenia) {
+		this.contrasenia = contrasenia;
+	}
+
+	public String getCorreo() {
+		return correo;
+	}
+
+	public void setCorreo(String correo) {
+		this.correo = correo;
+	}
+
+	public String getFoto() {
+		return foto;
+	}
+
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+
+}
