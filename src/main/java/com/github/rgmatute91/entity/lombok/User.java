bab13bda52c5458de320cd31c8a2011ee9931b92
Ronny Matute <rgmatute91@gmail.com>
2022-01-05 22:51:20 -0500

    Se utilizo lombok en la entidad User

--- src/main/java/com/github/rgmatute91/entity/lombok/User.java
+++ src/main/java/com/github/rgmatute91/entity/lombok/User.java
@@ -1,6 +1,8 @@
 package com.github.rgmatute91.entity.lombok;
 
+import lombok.Data;
 
+@Data
 public class User {
 
 	private String usuario;
@@ -14,53 +16,7 @@ public class User {
 	private String correo;
 
 	private String foto;
-
-	public String getUsuario() {
-		return usuario;
-	}
-
-	public void setUsuario(String usuario) {
-		this.usuario = usuario;
-	}
-
-	public String getNombre() {
-		return nombre;
-	}
-
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-
-	public String getApellidos() {
-		return apellidos;
-	}
-
-	public void setApellidos(String apellidos) {
-		this.apellidos = apellidos;
-	}
-
-	public String getContrasenia() {
-		return contrasenia;
-	}
-
-	public void setContrasenia(String contrasenia) {
-		this.contrasenia = contrasenia;
-	}
-
-	public String getCorreo() {
-		return correo;
-	}
-
-	public void setCorreo(String correo) {
-		this.correo = correo;
-	}
-
-	public String getFoto() {
-		return foto;
-	}
-
-	public void setFoto(String foto) {
-		this.foto = foto;
-	}
+	
+	
 
 }
